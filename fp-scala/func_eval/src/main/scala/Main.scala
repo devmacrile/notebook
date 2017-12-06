27230f0d8a7d4041c8c1b37864d0c95b61a78e1b
devmacrile <devin@farmlogs.com>
2017-12-05 20:00:09 -0500

    Finished simple recursive exercises.

--- fp-scala/func_eval/src/main/scala/Main.scala
+++ fp-scala/func_eval/src/main/scala/Main.scala
@@ -7,4 +7,26 @@ class Main {
     iter(c, r)
   }
 
+
+  def balance(chars: List[Char]) = {
+    def balance_iter(chars: List[Char], open: Int): Boolean = {
+      if (chars.isEmpty && open == 0) true
+      else if ((chars.isEmpty && open != 0) || (open >= 0 && chars.head == ')')) false
+      else
+        chars.head match {
+          case '(' => balance_iter(chars.tail, open + 1)
+          case ')' => balance_iter(chars.tail, open - 1)
+          case _ => balance_iter(chars.tail, open)
+        }
+    }
+    balance_iter(chars, 0)
+  }
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 0
+    else if (money < 0 || coins.isEmpty) 0
+    else
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
+
 }
